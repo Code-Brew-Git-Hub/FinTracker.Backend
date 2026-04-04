56306fdfb5fe8f1e1f3804b18092cf4a986609d1
trydov1k <mdimon00@mail.ru>
2026-04-04 12:15:06 +0500

    Реализовал проект парсера (пока только для csv файлов)

---
+++ FinTracker.Backend/FinTracker.Parser/PdfParser.cs
@@ -0,0 +1,11 @@
+﻿using FinTracker.Domain;
+
+namespace FinTracker.Parser;
+
+public class PdfParser
+{
+    public static IEnumerable<Transaction> ParsePDF(StreamReader reader)
+    {
+        throw new NotImplementedException();
+    }
+}
