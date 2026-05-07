03ed3384c030226a5068aefdd785edd86ff9f2a2
Дмитрий <mdimon00@mail.ru>
2026-05-07 08:56:22 +0500

    Сделал заготовку под контроллер AnaliticsController

---
+++ FinTracker.Backend/FinTracker.API/Controllers/AnalyticsController.cs
@@ -0,0 +1,36 @@
+﻿using FinTracker.Domain.Dtos.Analytics;
+using FinTracker.Domain.Dtos.Universal;
+using FinTracker.Domain.Enums;
+using FinTracker.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinTracker.API.Controllers;
+
+[ApiController]
+[Route("api/analytics")]
+public class AnalyticsController(IAnalyticsService analyticsService) : ControllerBase
+{
+    [HttpGet("summary")]
+    public async Task<ActionResult<ApiResponse<AnalyticsSummaryDto>>> GetSummary([FromQuery] AnalyticsFilterDto filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("by-category")]
+        public async Task<ActionResult<ApiResponse<IEnumerable<CategoryStatDto>>>> GetByCategory([FromQuery] AnalyticsFilterDto filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("by-tag")]
+        public async Task<ActionResult<ApiResponse<IEnumerable<TagStatDto>>>> GetByTag([FromQuery] AnalyticsFilterDto filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("by-time")]
+        public async Task<ActionResult<ApiResponse<IEnumerable<TimeStatDto>>>> GetByTime([FromQuery] AnalyticsFilterDto filter, [FromQuery] TimeGrouping grouping)
+    {
+        throw new NotImplementedException();
+    }
+}
