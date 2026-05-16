2b27f887f7b262414656cb46f51654dc7089e8b4
Дмитрий <mdimon00@mail.ru>
2026-05-16 21:18:49 +0500

    Сделал честный MemoryCache

--- FinTracker.Data/Services/CategoryService.cs
+++ FinTracker.Data/Services/CategoryService.cs
@@ -1,10 +1,13 @@
 ﻿using FinTracker.Domain.Interfaces.Repositories;
 using FinTracker.Domain.Interfaces.Services;
 using FinTracker.Domain.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace FinTracker.Data.Services;
 
-public class CategoryService(ICategoryRepository categoryRepository) : ICategoryService
+public class CategoryService(
+    ICategoryRepository categoryRepository,
+    IMemoryCache memoryCache) : ICategoryService
 {
     public async Task<Category> CreateAsync(string name)
     {
@@ -17,11 +20,18 @@ public class CategoryService(ICategoryRepository categoryRepository) : ICategory
         await categoryRepository.AddAsync(newCategory);
         await categoryRepository.SaveChangesAsync();
 
+        memoryCache.Remove(name);
+
         return newCategory;
     }
 
     public async Task DeleteAsync(Guid id)
     {
+        var category = await categoryRepository.GetByIdAsync(id);
+
+        if (category != null)
+            memoryCache.Remove(category.Name);
+
         await categoryRepository.DeleteAsync(id);
         await categoryRepository.SaveChangesAsync();
     }
@@ -40,9 +50,14 @@ public class CategoryService(ICategoryRepository categoryRepository) : ICategory
     {
         var entity = await categoryRepository.EnsureExistsAsync(id);
 
+        var oldName = entity.Name;
+
         entity.Name = name;
         await categoryRepository.UpdateAsync(entity);
         await categoryRepository.SaveChangesAsync();
+
+        memoryCache.Remove(oldName);
+        memoryCache.Remove(name);
 
         return entity;
     }
